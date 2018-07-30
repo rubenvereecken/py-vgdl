ee2d7b0f3c20c2b567d63bb15fe598feae175126
Ruben Vereecken <rubenvereecken@gmail.com>
2018-07-30 15:06:26 +0100

    Improved pybrain interface for discrete-state tasks

--- vgdl/interfaces/pybrain.py
+++ vgdl/interfaces/pybrain.py
@@ -3,8 +3,9 @@ import numpy as np
 from pybrain.rl.environments.environment import Environment
 from pybrain.rl.environments.episodic import EpisodicTask
 
-from typing import List
+from typing import List, Tuple, Dict
 from vgdl.core import Action
+from vgdl.state import StateObserver, Observation
 
 
 class VGDLPybrainEnvironment(Environment):
@@ -37,8 +38,6 @@ class VGDLPybrainEnvironment(Environment):
 
         # Some observers need pygame initialised first
         self.init_observation = state_handler.get_observation()
-        print(self.init_game_state, self.init_observation)
-        print(self.action_set)
         self.outdim = len(self.init_observation)
 
 
@@ -59,13 +58,17 @@ class VGDLPybrainEnvironment(Environment):
 
 
     def performAction(self, action: Action):
+        if not isinstance(action, Action):
+            action = self.action_set[int(action)]
+        assert isinstance(action, Action)
         self.game.tick(action)
 
 
 class VGDLPybrainTask(EpisodicTask):
-    # def __init__(self):
-    #     super()
-
+    """
+    Our Task is really just a wrapper for the environment, which represents
+    a specific game with reward function and everything and is itself episodic.
+    """
 
     def getReward(self):
         # TODO this is actually accumulated, but let's roll for now
@@ -73,6 +76,29 @@ class VGDLPybrainTask(EpisodicTask):
         score = self.env.game.score
         return score
 
+    def isFinished(self):
+        return self.env.game.ended
+
     # A Pybrain task is supposed to constrain the observations
     # def getObservation(self):
     #     pass
+
+
+import wrapt
+
+class SparseMDPObserver(wrapt.ObjectProxy):
+    """
+    Assigns each state a unique id so states become discrete.
+    Used when feature domains are sparse (and hence probably discrete).
+    Observations must be hashable.
+    """
+    def __init__(self, wrapped):
+        super().__init__(wrapped)
+        self._obs_cache: Dict[Observation, int] = {}
+
+    def get_observation(self):
+        obs = self.__wrapped__.get_observation()
+        if obs not in self._obs_cache:
+            self._obs_cache[obs] = [len(self._obs_cache)]
+        return self._obs_cache[obs]
+
