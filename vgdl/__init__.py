0f231d6aef62e28adc2afe6271e9077f6e1df5b2
Ruben Vereecken <rubenvereecken@gmail.com>
2019-01-08 11:17:24 +0000

    Add core classes to main package

--- vgdl/__init__.py
+++ vgdl/__init__.py
@@ -3,4 +3,8 @@ from .parser import VGDLParser
 
 __all__ = ['VGDLParser', 'ontology', 'registry']
 
+from .core import SpriteRegistry
+from .core import BasicGame, BasicGameLevel, GameState
+from .core import Action, ACTION
+from .core import Avatar, VGDLSprite, Immutable, Physics, Termination, Effect, FunctionalEffect
 
