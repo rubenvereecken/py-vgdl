5af57037c2dbf387e5319e6a5f6c0693f28f1ea7
Ruben Vereecken <rubenvereecken@gmail.com>
2019-06-20 10:27:33 +0100

    Migrating to pbr for setup

--- setup.py
+++ setup.py
@@ -1,30 +1,7 @@
-import os
-from setuptools import setup, find_packages
-
-# Feature-specific dependencies
-extras = {
-    'gym': ['gym'],
-    'pybrain': ['pybrain'],
-    'human': ['gym', 'gym_recording'],
-}
-
-# Also add an 'all' group which just install everything
-all_deps = []
-for group_name in extras:
-    all_deps += extras[group_name]
-extras['all'] = all_deps
+#!/usr/bin/env python
+from setuptools import setup
 
 setup(
-    name='vgdl',
-    version="1.0.1",
-    description='A video game description language (VGDL) built on top pf pygame',
-    author='Tom Schaul',
-    url='https://github.com/schaul/py-vgdl',
-    packages=find_packages(),
-    install_requires=['pygame'],
-    package_data={
-        # setuptools forces us to have data files within the respective package
-        'vgdl': ['games/', 'sprites/']
-    },
-    extras_require=extras
+    setup_requires=['pbr'],
+    pbr=True,
 )
